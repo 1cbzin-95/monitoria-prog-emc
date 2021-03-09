fcde56b32b3961a3145228829067a6e989d16e23
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-09 08:39:18 -0300

    add codigo aberto, relemprando apontadores e estruturas

---
+++ Programinhas-de-exemplo/Codigo-Aberto/novo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void bagunca(int *vetor,int tamanho);
+void mostraVetor(int vetor[],int tamanho);
+
+int main(int argc, char *argv[]){
+    int vetor[5]={1,2,3,4,5};
+    mostraVetor(vetor,5);
+    bagunca(vetor,5);
+    printf("------------------------\n");
+    mostraVetor(vetor,5);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void bagunca(int *vetor,int tamanho){
+    int vetorAux[tamanho];
+    for(int i=0;i<tamanho;i++){
+        vetorAux[i]=vetor[i+1];
+        if(i==4){
+            vetorAux[4]=vetor[4];
+        }
+    }
+    //passo de volta
+    for(int i=0;i<tamanho;i++){
+        vetor[i]=vetorAux[i];
+    }
+}
+void mostraVetor(int vetor[],int tamanho){
+    for(int i=0;i<tamanho;i++){
+        printf("posicao:%d == %d \n",i,vetor[i]);
+    }
+}
